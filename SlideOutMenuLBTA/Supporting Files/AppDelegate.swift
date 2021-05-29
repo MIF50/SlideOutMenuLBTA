e326451728d3e34272fcbf5ffd52a6930ceac82c
mohamed <mohamedibrahim1223@gmail.com>
2021-05-29 10:26:24 +0300

    adding gitignore.

---
+++ SlideOutMenuLBTA/Supporting Files/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SlideOutMenuLBTA
+//
+//  Created by MIF50 on 29/05/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        return true
+    }
+
+}
+
